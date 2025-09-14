40ff1e99ce997d30e23b98ea00dce1bdf8299b01
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2025-09-14 12:14:13 +0200

    feat: add new pages for About, Discover, Posts, Profile, Search, and Write; update Navbar menu items with new routes

--- src/actions/send-email.action.ts
+++ src/actions/send-email.action.ts
@@ -25,7 +25,7 @@ export async function sendEmailAction({
   const mailOptions = {
     from: process.env.NODEMAILER_USER,
     to,
-    subject: `Better Auth Prisma MongoDB Starter - ${subject}`,
+    subject: `Cognify, The best blog for students and  professional - ${subject}`,
     html: `
     <div style="${styles.container}">
       <h1 style="${styles.heading}">${subject}</h1>
